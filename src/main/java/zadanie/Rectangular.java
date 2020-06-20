1ad0672e3e249cbaf00a90a3bb453d3a016e415a
Lukasz <siennicki.lukasz95@gmail.com>
2020-06-20 14:00:30 +0200

    zadanie z figurami, dziedziczenie klasy Square

--- src/main/java/zadanie/Rectangular.java
+++ src/main/java/zadanie/Rectangular.java
@@ -1,20 +1,12 @@
 package zadanie;
 
-public class Rectangular implements Calculate {
-    private double a;
+public class Rectangular extends Square implements Calculate {
+
     private double b;
 
     public Rectangular(double a, double b) {
-        this.a = a;
+        super(a);
         this.b = b;
-    }
-
-    public double getA() {
-        return a;
-    }
-
-    public void setA(double a) {
-        this.a = a;
     }
 
     public double getB() {
@@ -27,13 +19,13 @@ public class Rectangular implements Calculate {
 
     @Override
     public void pole() {
-        double area = a * b;
+        double area = getA() * b;
         System.out.println("Pole prostokata wynosi: " + area);
     }
 
     @Override
     public void obwod() {
-        double circumference = 2 * a + 2 * b;
+        double circumference = 2 * getA() + 2 * b;
         System.out.println("Obwod prostokata wynosi: " + circumference);
     }
 }
